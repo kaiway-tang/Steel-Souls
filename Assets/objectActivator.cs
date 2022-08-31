f85174096d38d5365b4abfd9450bc4c51e9325fd
Kaiway Tang <86494513+kaiway-tang@users.noreply.github.com>
2022-08-30 19:23:38 -0700

    parallaxing and background swapping

--- Assets/objectActivator.cs
+++ Assets/objectActivator.cs
@@ -4,40 +4,92 @@ using UnityEngine;
 
 public class objectActivator : MonoBehaviour
 {
-    Transform plyrTrfm = Toolbox.playerTrfm;
+    Transform plyrTrfm;
     [SerializeField] obj[] objects;
+
+    public enum activeTypeEnum { leftOfZone, rightOfZone, aboveZone, belowZone }
 
     [System.Serializable]
     public class obj
     {
-        enum activeTypeEnum { leftOfZone, rightOfZone, aboveZone, belowZone}
-        activeTypeEnum activeType;
-        bool isActive;
-        GameObject[] objects;
+        public activeTypeEnum activeType;
+        public bool isActive;
+        public GameObject[] objects;
         public float pos, max, min;
+        public int checkTmr;
 
-
-        void updateStatus(Transform plyrTrfm)
+        public void updateStatus(Transform plyrTrfm)
         {
             if (activeType == activeTypeEnum.leftOfZone)
             {
                 if (plyrTrfm.position.y < max && plyrTrfm.position.y > min)
                 {
-                    //if (plyrTrfm.position.x < pos)
+                    if (isActive != plyrTrfm.position.x < pos)
+                    {
+                        isActive = plyrTrfm.position.x < pos;
+                        for (int i = 0; i < objects.Length; i++)
+                        {
+                            objects[i].SetActive(isActive);
+                        }
+                    }
+                }
+            }
+            else if (activeType == activeTypeEnum.rightOfZone)
+            {
+                if (plyrTrfm.position.y < max && plyrTrfm.position.y > min)
+                {
+                    if (isActive != plyrTrfm.position.x > pos)
+                    {
+                        isActive = plyrTrfm.position.x > pos;
+                        for (int i = 0; i < objects.Length; i++)
+                        {
+                            objects[i].SetActive(isActive);
+                        }
+                    }
+                }
+            }
+            else if (activeType == activeTypeEnum.belowZone)
+            {
+                if (plyrTrfm.position.x < max && plyrTrfm.position.x > min)
+                {
+                    if (isActive != plyrTrfm.position.y < pos)
+                    {
+                        isActive = plyrTrfm.position.y < pos;
+                        for (int i = 0; i < objects.Length; i++)
+                        {
+                            objects[i].SetActive(isActive);
+                        }
+                    }
+                }
+            }
+            else if (activeType == activeTypeEnum.aboveZone)
+            {
+                if (plyrTrfm.position.x < max && plyrTrfm.position.x > min)
+                {
+                    if (isActive != plyrTrfm.position.y > pos)
+                    {
+                        isActive = plyrTrfm.position.y > pos;
+                        for (int i = 0; i < objects.Length; i++)
+                        {
+                            objects[i].SetActive(isActive);
+                        }
+                    }
                 }
             }
         }
     }
     private void Start()
     {
+        plyrTrfm = Toolbox.playerTrfm;
         InvokeRepeating("checkPos", 0.5f ,0.5f);
     }
 
     private void checkPos()
     {
-        for (int i = 0; i < objects.Length; i++)
+        for (int i = 1; i < objects.Length; i++)
         {
-            
+            objects[i].updateStatus(plyrTrfm);
         }
     }
+
 }
--- Assets/parallax.cs
+++ Assets/parallax.cs
@@ -7,12 +7,18 @@ public class parallax : MonoBehaviour
     [SerializeField] Transform[] backgrounds;
     [SerializeField] Vector2[] ratios;
     [SerializeField] float rate;
-    Vector2 initPos, setPos;
+    Vector2 camInitPos, setPos;
+    Vector2[] initPos;
     Transform camTrfm;
     void Start()
     {
         camTrfm = Toolbox.camTrfm;
-        initPos = camTrfm.position;
+        camInitPos = camTrfm.position;
+        initPos = new Vector2[backgrounds.Length];
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            initPos[i] = backgrounds[i].position;
+        }
     }
 
     // Update is called once per frame
@@ -20,8 +26,8 @@ public class parallax : MonoBehaviour
     {
         for (int i = 0; i < backgrounds.Length; i++)
         {
-            setPos.x = camTrfm.position.x + ratios[i].x * (initPos.x - camTrfm.position.x) * rate;
-            setPos.y = camTrfm.position.y + ratios[i].y * (initPos.y - camTrfm.position.y) * rate;
+            setPos.x = initPos[i].x + ratios[i].x * (camTrfm.position.x - camInitPos.x) * rate;
+            setPos.y = initPos[i].y + ratios[i].y * (camTrfm.position.y - camInitPos.y) * rate;
             backgrounds[i].position = setPos;
         }
     }
