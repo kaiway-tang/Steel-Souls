38b5f161609f5689e6b6ae3a1c6810a94fa2bd33
Kaiway Tang <86494513+kaiway-tang@users.noreply.github.com>
2022-09-04 15:28:08 -0700

    door sprite mask edits, shelley enemy assets

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -4,21 +4,8 @@ using UnityEngine;
 
 public class test : testParent
 {
-    private void Start()
+    private void FixedUpdate()
     {
-        me();
-    }
-    public new void me()
-    {
-        Debug.Log("base");
-        base.me();
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        HPEntity HPScr;
-        if (HPScr = collision.GetComponent<HPEntity>())
-        {
-            HPScr.TakeDamage(3);
-        }
+        transform.Rotate(Vector3.forward*10);
     }
 }
