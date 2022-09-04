975274eae57f3a45a5b63d2c58f9b2e8414ccda0
Kaiway Tang <86494513+kaiway-tang@users.noreply.github.com>
2022-09-04 16:17:17 -0700

    enemy prefabs and enemy scripts

--- Assets/shelley.cs
+++ Assets/shelley.cs
@@ -4,25 +4,65 @@ using UnityEngine;
 
 public class shelley : enemy
 {
-    [SerializeField] Transform trfm, plyrTrfm;
-    int cd;
+    [SerializeField] SpriteRenderer rend;
+    [SerializeField] Sprite[] sprites; //0: default  1: ball
+    Transform plyrTrfm;
+    int cd, terrainLayerMask = 1 << 6;
+    RaycastHit2D hit;
+    bool every2;
+
+    private void Start()
+    {
+        jumpPower = 15;
+        plyrTrfm = Toolbox.playerTrfm;
+    }
+
     void FixedUpdate()
     {
         _FixedUpdate();
 
         if (cd > 0)
         {
+            if (cd == 126)
+            {
+                if (currentFacing == rightFace) SetVelX(9);
+                else SetVelX(-9);
+                rb.drag = 0;
+            }
+            if (cd < 50 && Mathf.Abs(trfm.position.x - plyrTrfm.position.x) > 5)
+            {
+                SetVelX(0);
+                rb.drag = 1;
+            } else
+            {
+                if (currentFacing == rightFace) trfm.Rotate(Vector3.forward * 15);
+                else trfm.Rotate(Vector3.forward * 15);
+            }
             cd--;
         }
-        if (Mathf.Abs(trfm.position.x - plyrTrfm.position.x) < 9 && Mathf.Abs(trfm.position.y - plyrTrfm.position.y) < 6)
+
+        if (every2) everyTwo();
+        every2 = !every2;
+    }
+    void everyTwo()
+    {
+        if (Mathf.Abs(trfm.position.x - plyrTrfm.position.x) < 10 && Mathf.Abs(trfm.position.y - plyrTrfm.position.y) < 6)
         {
-            RaycastHit2D hit = Physics2D.Linecast(trfm.position, plyrTrfm.position);
-            if (trfm.position.x - plyrTrfm.position.x > 0) FaceDir(leftFace);
-            else FaceDir(rightFace);
+            hit = Physics2D.Linecast(trfm.position, plyrTrfm.position, terrainLayerMask);
 
-            if (cd < 1)
+            if (!hit)
             {
-
+                if (trfm.position.x - plyrTrfm.position.x > 0) FaceDir(leftFace);
+                else FaceDir(rightFace);
+                if (Mathf.Abs(trfm.position.x - plyrTrfm.position.x) < 7)
+                {
+                    if (cd < 1)
+                    {
+                        Jump();
+                        rend.sprite = sprites[1];
+                        cd = 150;
+                    }
+                }
             }
         }
     }
